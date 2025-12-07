49a12b9d117a187a953e832bf2425233f006f0ed
Md. Sohorafuzzaman Shuvo <47665373+Shuvo82@users.noreply.github.com>
2025-12-07 22:20:04 +0600

    prob fix

--- best_flutter_ui_templates/lib/fitness_app/fitness_app_home_screen.dart
+++ best_flutter_ui_templates/lib/fitness_app/fitness_app_home_screen.dart
@@ -1,4 +1,4 @@
-import 'package:best_flutter_ui_templates/fitness_app/models/tabIcon_data.dart';
+import 'package:best_flutter_ui_templates/fitness_app/models/tab_icon_data.dart';
 import 'package:best_flutter_ui_templates/fitness_app/training/training_screen.dart';
 import 'package:flutter/material.dart';
 import 'bottom_navigation_view/bottom_bar_view.dart';
